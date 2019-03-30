b6ad99f889cd5801bfe0c4baabca8df51b64e186
Enocportillo503 <49107162+Enocportillo503@users.noreply.github.com>
2019-03-30 17:41:53 +0100

    creacion del navbar

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <navbar> </navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.vue"
+export default {
+  components: {
+    Navbar 
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
---
+++ src/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+<div> 
+ 
+
+<nav class="navbar navbar-expand-lg navbar-light " style="background:#F8ED57" >
+  <a class="navbar-brand" href="#"><img alt="Logo DBO" style="height:50px" src="images/logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active" style="width:100px">
+        <a class="nav-link" href=""> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">QUIÉNES SOMOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CÓMO AYUDAR</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACTO</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">DONACIONES</a>
+      </li>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      </ul>
+    
+  </div>
+</nav>
+
+
+</div>
+</template>
