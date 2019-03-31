144435f8a2b3464bd5a8235e25bf32e15e544dc2
Enocportillo503 <49107162+Enocportillo503@users.noreply.github.com>
2019-03-31 13:38:17 +0200

    se insertó logo debajo del fono y la frase, descripción de DBo ministerios, juntamente con la tabla de visión, misión, quiénes somos y su contenido.

--- src/Home.vue
+++ src/Home.vue
@@ -7,10 +7,83 @@
             <br>
             <br>
             <br>
+            <blockquote><center> Y el Rey responderá y les dirá: De cierto os digo que en cuanto lo hicisteis 
+                a uno de estos mis hermanos más <br> pequeños, a mí me lo hicisteis. Mateo 25:40</center> </blockquote> 
+           
             <br>
-            <p><center> Y el Rey responderá y les dirá: De cierto os digo que en cuanto lo hicisteis 
-                a uno de estos mis hermanos más <br> pequeños, a mí me lo hicisteis. Mateo 25:40</center> </p>
-        </div>
+            <br>
+             <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <center><img alt="Logo DBO" width="310px" height="143px" src="images/logob.jpeg"></center>
+            <br>
+            <br>
+            <p> <center><b>Asociación DBO Ministerios se ha dedicado principalmente al trabajo en calles y
+            <br>
+                 áreas de riesgo, nunca se ha pensado en menguar la ayuda brindada al prójimo,
+            <br>
+                  ya que todo está fundamentado en la base bíblica de Mateo 25: 36 - 40.</b></center></p>
+           
+            </div>
+             <br>
+            <br>
+             <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+             <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+       <TABLE align="center" bottom="middle">> 
+     <TR> 
+          <td><center><img width="203px" height="202px" src="images/vision.jpeg"></center> </td> <td> <img width="204px" height="203px" src="images/mision.jpeg"> </td> <td> <img width="207" height="202" src="images/quienessomos.jpeg"> </td>
+     </TR> <TR>
+          <td><center>VISIÓN</center>
+              <br>
+             <center>  Alcanzar el desarrollo integral y
+              <br>
+               espiritual en personas de diferentes
+               <br>
+                condiciones, dificultades y
+               <br> 
+                 necesidades físicas, materiales y
+                <br> 
+                  emocionales de nuestra sociedad. </center></td> <td><center>MISIÓN</center>
+                  <br>
+                  <center>Somos un Ministerio Cristocéntrico
+                    <br>  
+                        que busca transformar y
+                    <br>
+                        empoderar vidas de los más
+                    <br>
+                         necesitados y rechazados por la
+                    <br> 
+                          sociedad convencional.</center></td> <td><center>¿QUIÉNES SOMOS?</center> 
+                    <br>
+                          <center>Asociación DBO está conformada 
+                    <br>
+                              por un grupo personas
+                    <br>
+                               voluntarias, quienes son encargados
+                    <br>
+                                de coordinar las actividades
+                    <br>    
+                                 que realiza cada uno de los
+                    <br>    
+                                  ministerios.
+                              </center></td>
+</TR> 
+</TABLE>
+            
     </div>
 </template>
 
@@ -31,7 +104,19 @@
         color:#fff;      
       
     }
-    .header p{
+    .header blockquote{
         color: #fff;
     }
+    
+    
+    .header p{
+        color:#6F6F6F;
+    }
+
+    .header table{
+        font-family: Roboto;
+        color:#6F6F6F;
+    }  
+  
+   
 </style>
--- src/Navbar.vue
+++ src/Navbar.vue
@@ -3,7 +3,7 @@
  
 
 <nav class="navbar navbar-expand-lg navbar-light " style="background:#F8ED57" >
-  <a class="navbar-brand" href="#"><img alt="Logo DBO" style="height:50px" src="images/logo.png"></a>
+  <a class="navbar-brand" href="#"><img alt="Logo DBO" style="height:105px" src="images/logo.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
