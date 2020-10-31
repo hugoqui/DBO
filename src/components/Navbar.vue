44d289e831671f9ae2eb53bb699406c84c3ffa37
JUAN YANEZ <Jeremy Yanez>
2020-10-31 11:36:34 -0600

    actualizacion

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
 <div> 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-yellow fixed-top " >
+  <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top " >
     <a class="navbar-brand" href="#"><img alt="Logo DBO" style="height:65px" src="images/black_logo.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -19,12 +19,12 @@
           <a class="nav-link" href="#">HOW CAN I HELP?</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">SPONSORS</a>
+          <a class="nav-link" href="#">WHAT WE DO</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">CONTACT</a>
         </li>
-        <li class="nav-item border" style="background-color:rgba(0,0,0,0.8)">
+        <li class="nav-item border bg-yellow">
           <a class="nav-link" href="#">DONATIONS</a>
         </li>
         
@@ -37,5 +37,5 @@
 
 <style>
   .nav-item{ margin-left: 20px; margin-right: 20px}
-  .nav-link {color: #fff !important;}
+  .nav-link {color: #000 !important; font-family:Arial;}
 </style>
